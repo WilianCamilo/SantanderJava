1dbe5e30d18940a0018ae97b4b889430ba22963b
wilian camilo <wiliancamilos1@gmail.com>
2024-04-24 23:04:34 -0300

    Revert "commit teste"
    This reverts commit 4667869994f180efdbb1e72b74da0751062e3519

--- java/src/App.java
+++ java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Entendendo ainda mais o GIT com a IDE");
+        System.out.println("Hello, World!!");
     }
 }
