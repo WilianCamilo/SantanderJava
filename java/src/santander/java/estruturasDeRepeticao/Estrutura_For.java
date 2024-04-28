88f17e29edd013234052bcd582a584cc2f08a7de
wilian camilo <wiliancamilos1@gmail.com>
2024-04-28 11:38:28 -0300

    Estruturas de repetições

---
+++ java/src/santander/java/estruturasDeRepeticao/Estrutura_For.java
@@ -0,0 +1,9 @@
+package santander.java.estruturasDeRepeticao;
+
+public class Estrutura_For {
+    public static void main (String[] args) {
+        //preciso contar 20 carneirinhos
+        for (int carneirinhos = 1 ; carneirinhos <= 20 ; carneirinhos ++ )
+        System.out.println("Contando carneirinhos: " + carneirinhos);
+    }
+}
